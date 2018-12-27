f2a746df65a64db816af5f2cc1b685dd94749c6e
1146012448@qq.com <1146012448@qq.com>
2018-12-27 09:16:01 +0800

    fix

--- RunMain/run_interface.py
+++ RunMain/run_interface.py
@@ -3,7 +3,7 @@ from InterfaceAuto.common.run_suite import RunSuite
 
 if __name__ == '__main__':
     project_name = "Intelligent_mediation"
-    RunSuite().run(project_name)
+    RunSuite().run(project_name,send_email=True)
 
     # import time
     # time.sleep(2)
