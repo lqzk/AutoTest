a00cc54547843c44e3ecef7ddc35ed6ef0b55f58
1146012448@qq.com <1146012448@qq.com>
2019-01-23 10:14:45 +0800

    file_handle_service 接口自动化

---
+++ RunMain/interface_auto/run_interface_File_handle_service.py
@@ -0,0 +1,18 @@
+from InterfaceAuto.common.run_suite import RunSuite
+
+
+if __name__ == '__main__':
+    project_name = "File_handle_service"
+    RunSuite().run(project_name,send_email=True)
+
+    # import time
+    # time.sleep(2)
+    #
+    # project_name = "Intelligent_mediation_web"
+    # RunSuite().run(project_name,send_email=True)
+
+
+
+
+
+
